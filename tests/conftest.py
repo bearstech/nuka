42338bc4dc7b43209055db214c359a69aa46e696
Gael Pasgrimaud <gael@gawel.org>
2017-04-14 11:50:34 +0200

    add net

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,5 +2,6 @@
 import os
 from nuka import config
 
+config['inventory_modules'].append('nuka.inventory.net')
 config['templates'].append(os.path.join(
         os.path.dirname(__file__), 'templates'))
