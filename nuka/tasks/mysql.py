de2fab2c1af5b9501400d00a4b7f58b75c775f21
Gael Pasgrimaud <gael@gawel.org>
2017-04-25 16:31:26 +0200

    added mysql.set_root_password()

--- nuka/tasks/mysql.py
+++ nuka/tasks/mysql.py
@@ -2,6 +2,7 @@
 """
 mysql related tasks
 """
+import logging as log
 import os
 import codecs
 import getpass
@@ -50,6 +51,36 @@ class my_cnf(Task):
             'password={password}\n').format(**self.args)
         diff = self.texts_diff(old_data, data, fromfile=dst)
         return dict(rc=0, diff=diff)
+
+
+class set_root_password(my_cnf):
+    """set mysql root password and store it in ~/.my.cnf"""
+
+    def __init__(self, password=None, **kwargs):
+        kwargs.update(password=password, switch_user='root')
+        super(set_root_password, self).__init__(**kwargs)
+
+    def do(self):
+        new_password = self.args['password']
+        old_password = None
+
+        # get old password
+        dst = os.path.expanduser('/root/.my.cnf')
+        if os.path.isfile(dst):
+            with codecs.open(dst, 'r', 'utf8') as fd:
+                for line in fd:
+                    line = line.strip()
+                    if line.startswith('password'):
+                        old_password = line.split('=')[1].strip()
+
+        if old_password != new_password:
+            # change password
+            cmd = ['mysqladmin', 'password', new_password]
+            if old_password:
+                cmd[1:1] = ['--password=' + old_password]
+            self.sh(cmd)
+        # forward to my_cnf to update /root/.my.cnf
+        return super(set_root_password, self).do()
 
 
 class create_db(Task):
